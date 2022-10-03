750903b71f41ae9422419935481a4ec58e2ff8cc
longe <a0691401@126.com>
2022-10-03 14:54:47 +0800

    1

---
+++ componentization/src/main/java/net/itgoo/componentization/application/CZModuleApplicationHelper.java
@@ -0,0 +1,37 @@
+package net.itgoo.componentization.application;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by apple on 17/1/3.
+ */
+
+public class CZApplicationHelper {
+
+    private static List<CZModuleApplicationInitializer> sApplications = new ArrayList<>();
+
+    public static void register(CZModuleApplicationInitializer czModuleApplicationInitializer) {
+        sApplications.add(czModuleApplicationInitializer);
+    }
+
+    public static void initApplications(Context context) {
+        for (CZModuleApplicationInitializer applicationInitializer : sApplications) {
+            applicationInitializer.init(context);
+        }
+    }
+
+    public static void deInitApplications(Context context) {
+        for (CZModuleApplicationInitializer applicationInitializer : sApplications) {
+            applicationInitializer.deInit(context);
+        }
+    }
+
+    public static void attachApplications(Context context) {
+        for (CZModuleApplicationInitializer applicationInitializer : sApplications) {
+            applicationInitializer.attach(context);
+        }
+    }
+}
